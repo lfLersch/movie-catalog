9863b649ef16ad888903592862101ff1a31915cb
Luiz Felipe Lersch <luizflersch@gmail.com>
2022-04-26 11:41:32 -0300

    feat: add project scaffold

---
+++ lib/screens/catalog-screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CatalogScreen extends StatefulWidget {
+  static const String id = 'catalog_screen';
+  const CatalogScreen({Key? key}) : super(key: key);
+
+  @override
+  _CatalogScreenState createState() => _CatalogScreenState();
+}
+
+class _CatalogScreenState extends State<CatalogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/movie-screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatefulWidget {
+  static const String id = 'movie_screen';
+  const MovieScreen({Key? key}) : super(key: key);
+
+  @override
+  _MovieScreenState createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
